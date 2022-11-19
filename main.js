a82fcead11457a79bb3998a143847c78721ffdd4
Dylan Spyer <dylanspyer@gmail.com>
2022-11-19 16:13:56 -0600

    added button styling and clear button functionality

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 //create a function that creates 16 * 16 div elements with a class
 //style that class to be a small cube
 const gridContainer = document.querySelector(".gridContainer");
+const clearButton = document.querySelector(".clearButton");
+
+//Create the grid
 function createGridBoxes() {
   for (let i = 1; i <= 256; i++) {
-    let box = document.createElement("div");
+    let gridCell = document.createElement("div");
     let color = [, "#3C9EE7", "#E7993C", "#E73C99", "#3CE746", "#E7993C"];
-    box.className = "box";
-    gridContainer.appendChild(box);
-    box.style.cssText =
-      "border: 1px solid black; height 25px; width 25px; padding: 20px";
-    box.addEventListener("click", function () {
-      box.style.background = color[Math.floor(Math.random() * color.length)];
+    gridCell.className = "gridCell";
+    gridContainer.appendChild(gridCell);
+    gridCell.addEventListener("mouseover", function () {
+      gridCell.style.background =
+        color[Math.floor(Math.random() * color.length)];
     });
   }
 }
+
+//Clear button
+function clearGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  createGridBoxes();
+}
+clearButton.addEventListener("click", clearGrid);
 
 createGridBoxes();
